eb0375cc82593a07b5b723fa610b2d2fe4eab82f
NinjasWeb <hola@ninjasweb.com>
2023-07-11 15:47:02 -0500

    debuggin 3

--- src/app/page.js
+++ src/app/page.js
@@ -10,32 +10,32 @@ export default function Home() {
   return (
       <main className={styles.main}>
         {/* Hero */}
-        {/* <section className={styles.hero}>
+        <section className={styles.hero}>
           <Image src={TamalPng.src} width={120} height={120} alt="tamal tolimense"/>
           <h1>Tamales <br></br><span>TOLIMENSES</span></h1>
           <CallToAction>PEDIR AHORA</CallToAction>
-        </section> */}
+        </section>
         {/* Products */}
-        {/* <section className={styles.products}>
+        <section className={styles.products}>
           <h2>Nuestros Tamales</h2>
           <p>Tenemos diferentes tamales para todos los gustos. <br></br> Escoge el que prefieras.</p>
           <ProductGrid/>
-        </section> */}
+        </section>
         {/* Aviso */}
         <Tape/>
         {/* Horarios */}
-        {/* <section className={styles.times}>
+        <section className={styles.times}>
           <h3>Horarios:</h3>
           <h4>SAB-DOM-FEST</h4>
           <h4>7:00 AM - 12:00 PM</h4>
           <p style={{display: 'flex', margin: '0 auto', alignItems: 'center'}}><Image src={LocationIcon} width={30} height={30} alt='Ibagué'/> Ibagué, Tolima</p>
-        </section> */}
+        </section>
         {/* Call to Action */}
-        {/* <section className={styles.cta}>
+        <section className={styles.cta}>
           <h3 style={{color: 'var(--white)'}}>El verdadero</h3>
           <h3 style={{color: 'var(--yellow)', marginBottom: '1rem'}}>Tamal Tolimense</h3>
           <CallToAction>PEDIR AHORA</CallToAction>
-        </section> */}
+        </section>
         {/* Aviso */}
         <Tape/>
       </main>
