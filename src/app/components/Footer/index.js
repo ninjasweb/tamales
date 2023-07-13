bff2caf035bf59b26ccf08de416ba696270d7600
NinjasWeb <hola@ninjasweb.com>
2023-07-13 09:22:21 -0500

    adding credits footer responsive

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -2,7 +2,10 @@ import styles from './footer.module.css'
 
 const Footer = () => {
   return ( 
-    <footer className={styles.footer}>2023 Todos los derechos reservados | Diseño: <a href="https://ninjasweb.com" target="_blank">Ninjasweb</a></footer>
+    <footer className={styles.footer}>
+      <div>2023 Todos los derechos reservados</div><br></br>
+      <div>Diseño: <a href="https://ninjasweb.com" target="_blank">Ninjasweb</a></div>
+    </footer>
    )
 }
  
