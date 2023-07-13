2c10846bbe9c9fe25b2a9f3891a9112b0e498331
NinjasWeb <hola@ninjasweb.com>
2023-07-13 12:43:20 -0500

    Header responsive

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,17 +1,38 @@
-'use client'
+import { useState, useEffect } from 'react'
 import styles from './header.module.css'
-import CallToAction from "../Buttons/CallToAction"
+import CallToAction from '../Buttons/CallToAction'
 import Image from 'next/image'
 import TamalesLogo from '@/assets/img/logo.svg'
+import TamalesLogoAlter from '@/assets/img/logo_alternate.svg'
 import Link from 'next/link'
 
 const Header = () => {
-  return ( 
+  const [isLogoAlter, setIsLogoAlter] = useState(false)
+  const [logoSize, setLogoSize] = useState({ width: 385, height: 25 })
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth
+      setIsLogoAlter(screenWidth < 750)
+      setLogoSize(screenWidth < 750 ? { width: 130, height: 35 } : { width: 385, height: 25 })
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  const logoSrc = isLogoAlter ? TamalesLogoAlter : TamalesLogo
+
+  return (
     <>
       <header className={styles.header}>
         <div className={styles.container}>
           <Link href="/">
-            <Image src={TamalesLogo} width={385} height={25} alt='TamalesTolimenses.co'/>
+            <Image src={logoSrc} width={logoSize.width} height={logoSize.height} alt="TamalesTolimenses.co" />
           </Link>
           <nav className={styles.nav}>
             <ul>
@@ -24,7 +45,7 @@ const Header = () => {
         </div>
       </header>
     </>
-   )
+  )
 }
- 
+
 export default Header
